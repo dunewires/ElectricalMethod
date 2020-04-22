d573e6fc3d9fd0a14b8b99d278d629345d32a472
nfelt <felt@fas.harvard.edu>
2020-04-21 22:38:59 -0400

    removed sdk's local gitignore's

---
+++ firmware/source/sdk/dwaFsbl/Debug/src/image_mover.d
@@ -0,0 +1,76 @@
+src/image_mover.o src/image_mover.o: ../src/image_mover.c ../src/fsbl.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xil_printf.h ../src/pcap.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xdevcfg.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xdevcfg_hw.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../src/fsbl_debug.h \
+ /home/nate/projects/duneWireTension/ElectricalMethod/firmware/source/sdk/design_1_wrapper_hw_platform_0/ps7_init.h \
+ ../src/image_mover.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../src/fsbl_hooks.h ../src/md5.h \
+ ../../dwaFsbl_bsp/ps7_cortexa9_0/include/xil_types.h
+
+../src/fsbl.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../src/pcap.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xdevcfg.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xdevcfg_hw.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../src/fsbl_debug.h:
+
+/home/nate/projects/duneWireTension/ElectricalMethod/firmware/source/sdk/design_1_wrapper_hw_platform_0/ps7_init.h:
+
+../src/image_mover.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../src/fsbl_hooks.h:
+
+../src/md5.h:
+
+../../dwaFsbl_bsp/ps7_cortexa9_0/include/xil_types.h:
---
+++ firmware/source/sdk/lwip_ref/Release/src/iic_phyreset.d
@@ -0,0 +1,19 @@
+src/iic_phyreset.o src/iic_phyreset.o: ../src/iic_phyreset.c \
+ ../../lwip_echo1_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../lwip_echo1_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../lwip_echo1_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../lwip_echo1_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../lwip_echo1_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../lwip_echo1_bsp/ps7_cortexa9_0/include/bspconfig.h
+
+../../lwip_echo1_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../lwip_echo1_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../lwip_echo1_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../lwip_echo1_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../lwip_echo1_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../lwip_echo1_bsp/ps7_cortexa9_0/include/bspconfig.h:
