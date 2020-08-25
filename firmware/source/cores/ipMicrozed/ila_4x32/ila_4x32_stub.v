c6b0ff511eb2278bef9ce6dc3e0382371d0e7336
nfelt <felt@fas.harvard.edu>
2020-08-25 16:34:41 -0400

    added ila core

---
+++ firmware/source/cores/ipMicrozed/ila_4x32/ila_4x32_stub.v
@@ -0,0 +1,24 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
+// Date        : Thu Jul 16 10:53:10 2020
+// Host        : lt2 running 64-bit CentOS Linux release 7.6.1810 (Core)
+// Command     : write_verilog -force -mode synth_stub
+//               /home/nate/projects/duneWireTension/vivadoProjects/project_2/project_2.srcs/sources_1/ip/ila_4x32/ila_4x32_stub.v
+// Design      : ila_4x32
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-2
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "ila,Vivado 2019.2" *)
+module ila_4x32(clk, probe0, probe1, probe2, probe3)
+/* synthesis syn_black_box black_box_pad_pin="clk,probe0[31:0],probe1[31:0],probe2[31:0],probe3[31:0]" */;
+  input clk;
+  input [31:0]probe0;
+  input [31:0]probe1;
+  input [31:0]probe2;
+  input [31:0]probe3;
+endmodule
