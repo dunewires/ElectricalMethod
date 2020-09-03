34fddd4a68119c8029247205d5b0c39c7a950770
James Battat <jbattat@wellesley.edu>
2020-09-03 11:16:14 -0400

    Revert "enable digipot setting in daq"
    This reverts commit 33d1ae0f4ab3412901e0f9c08ded89ea5d21fa6e.

--- DAQ/python/dwaGoGo.py
+++ DAQ/python/dwaGoGo.py
@@ -4,7 +4,7 @@ print('\n\n======= dwaReset() ===========')
 dwa.dwaReset(verbose=1)
 
 print('\n\n======= dwaConfig() ===========')
-dwa.dwaConfig(verbose=0, configFile="dwaConfigWC.ini")
+dwa.dwaConfig(verbose=0, configFile="dwaConfig.ini")
 #dwa.dwaConfig(verbose=0, configFile="dwaConfigSingleFreq.ini")
 
 print('\n\n======= dwaStart() ===========')
