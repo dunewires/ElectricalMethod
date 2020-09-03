e2fdd1c5b2777d79575be42947921ebb5e028a07
James Battat <jbattat@wellesley.edu>
2020-09-03 11:27:45 -0400

    Revert "Revert "dpot tested""
    This reverts commit 04711970214673363197029854864682ff23359b.

--- firmware/source/hdl/dpotInterface.vhd
+++ firmware/source/hdl/dpotInterface.vhd
@@ -37,11 +37,23 @@ entity dpotInterface is
 end entity dpotInterface;
 
 architecture STRUCT of dpotInterface is
-	type dpotState_type is (idle_s, shiftBits_s, loadDaqReg_s);
+
+	COMPONENT ila_4x32
+		PORT (
+			clk : IN STD_LOGIC;
+
+			probe0 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			probe1 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			probe2 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			probe3 : IN STD_LOGIC_VECTOR(31 DOWNTO 0)
+		);
+	END COMPONENT ;
+
+	type dpotState_type is (idle_s, shiftBitsIn_s, loadDpotReg_s, shiftBitsOut_s, loadDaqReg_s);
 	signal dpotState : dpotState_type := idle_s;
 
-	signal shiftReg                        : std_logic_vector(35 downto 0);
-	signal serialString, serialStringLatch : SLV_VECTOR_TYPE(1 downto 0)(35 downto 0) := (
+	signal shiftReg     : std_logic_vector(35 downto 0);
+	signal serialString : SLV_VECTOR_TYPE(1 downto 0)(35 downto 0) := (
 			'1' & x"80" & '1' & x"80" & '1' & x"80" & '1' & x"80",
 			'0' & x"80" & '0' & x"80" & '0' & x"80" & '0' & x"80"
 	);
@@ -49,16 +61,30 @@ architecture STRUCT of dpotInterface is
 	signal clkEn,clkEn_del : std_logic                    := '0';
 	signal writeReg        : std_logic                    := '0';
 	signal update          : std_logic_vector(1 downto 0) := (others => '0');
+	signal dwaClk2         : std_logic                    := '0';
 begin
+
+	BUFR_inst : BUFR
+		generic map (
+			BUFR_DIVIDE => "5",      -- Values: "BYPASS, 1, 2, 3, 4, 5, 6, 7, 8"
+			SIM_DEVICE  => "7SERIES" -- Must be set to "7SERIES"
+		)
+		port map (
+			O   => dwaClk2, -- 1-bit output: Clock output port
+			CE  => '1',     -- 1-bit input: Active high, clock enable (Divided modes only)
+			CLR => '0',     -- 1-bit input: Active high, asynchronous clear (Divided modes only)
+			I   => dwaClk10 -- 1-bit input: Clock buffer input driven by an IBUF, MMCM or local interconnect
+		);
+
 	ODDR_SCK : ODDR
 		generic map(
 			DDR_CLK_EDGE => "OPPOSITE_EDGE", -- "OPPOSITE_EDGE" or "SAME_EDGE"
 			INIT         => '0',             -- Initial value for Q port ('1' or '0')
 			SRTYPE       => "SYNC")          -- Reset Type ("ASYNC" or "SYNC")
 		port map (
-			Q  => sck,      -- 1-bit DDR output
-			C  => dwaClk10, -- 1-bit clock input
-			CE => '1',      -- 1-bit clock enable input
+			Q  => sck,     -- 1-bit DDR output
+			C  => dwaClk2, -- 1-bit clock input
+			CE => '1',     -- 1-bit clock enable input
 			D1 => '0',
 			D2 => clkEn,
 			R  => '0', -- 1-bit reset input
@@ -84,9 +110,9 @@ begin
 	shdn_b <= '1';
 	sdo    <= shiftReg(shiftReg'left);
 
-	dpotState_seq : process (dwaClk10)
+	dpotState_seq : process (dwaClk2)
 	begin
-		if rising_edge(dwaClk10) then
+		if rising_edge(dwaClk2) then
 			--default 
 			cs_b  <= '0';
 			clkEn <= '0';
@@ -95,15 +121,26 @@ begin
 			case (dpotState) is
 
 				when idle_s =>
-					shiftCnt <= x"47"; --number of bits to  shift
+					shiftCnt <= x"23"; --number of bits to  shift
 					if update /= "00" then
 						-- dpot registers 1 are priority
 						shiftReg  <= serialString(1) when update(1) else serialString(0);
-						dpotState <= shiftBits_s;
+						dpotState <= shiftBitsIn_s;
 					end if;
 
-				when shiftBits_s =>
-					cs_b     <= '1' when shiftCnt = 34; -- load after 36 bits are shifted
+				when shiftBitsIn_s =>
+					clkEn    <= '1';
+					shiftCnt <= shiftCnt -1;
+					if shiftCnt = 0 then --busy shifting, come back later if you want to update
+						dpotState <= loadDpotReg_s;
+					end if;
+
+				when loadDpotReg_s =>
+					cs_b      <= '1';   -- load after 36 bits are shifted
+					shiftCnt  <= x"23"; --number of bits to  shift
+					dpotState <= shiftBitsOut_s;
+
+				when shiftBitsOut_s =>
 					clkEn    <= '1';
 					shiftCnt <= shiftCnt -1;
 					if shiftCnt = 0 then --busy shifting, come back later if you want to update
@@ -111,23 +148,41 @@ begin
 					end if;
 
 				when loadDaqReg_s =>
-					if shiftReg(35) = '1' then -- data is from address 1
-						toDaqReg.senseWireGain(7) <= shiftReg(34 downto 27);
-						toDaqReg.senseWireGain(6) <= shiftReg(25 downto 18);
-						toDaqReg.senseWireGain(5) <= shiftReg(16 downto 9);
-						toDaqReg.senseWireGain(4) <= shiftReg(7 downto 0);
-					else
-						toDaqReg.senseWireGain(3) <= shiftReg(34 downto 27);
-						toDaqReg.senseWireGain(2) <= shiftReg(25 downto 18);
-						toDaqReg.senseWireGain(1) <= shiftReg(16 downto 9);
-						toDaqReg.senseWireGain(0) <= shiftReg(7 downto 0);
+					if clkEn = '0' then            -- wait for last bit to be shifted in
+						if shiftReg(35) = '1' then -- data is from address 1
+							toDaqReg.senseWireGain(7) <= shiftReg(34 downto 27);
+							toDaqReg.senseWireGain(6) <= shiftReg(25 downto 18);
+							toDaqReg.senseWireGain(5) <= shiftReg(16 downto 9);
+							toDaqReg.senseWireGain(4) <= shiftReg(7 downto 0);
+						else
+							toDaqReg.senseWireGain(3) <= shiftReg(34 downto 27);
+							toDaqReg.senseWireGain(2) <= shiftReg(25 downto 18);
+							toDaqReg.senseWireGain(1) <= shiftReg(16 downto 9);
+							toDaqReg.senseWireGain(0) <= shiftReg(7 downto 0);
+						end if;
+						dpotState <= idle_s;
 					end if;
-					dpotState <= idle_s;
 
 				when others =>
 					dpotState <= idle_s;
 			end case;
 		end if;
 	end process dpotState_seq;
+
+	ila_4x32_inst : ila_4x32
+		PORT MAP (
+			clk => dwaClk2,
+
+			probe0              => (others => '0'),
+			probe1(31 downto 6) => (others => '0'),
+			probe1(5)           => update(1),
+			probe1(4)           => Sdi,
+			probe1(3)           => Sdo,
+			probe1(2)           => Cs_b,
+			probe1(1)           => update(0),
+			probe1(0)           => clkEn,
+			probe2              => (others => '0'),
+			probe3              => (others => '0')
+		);
 
 end architecture STRUCT;
