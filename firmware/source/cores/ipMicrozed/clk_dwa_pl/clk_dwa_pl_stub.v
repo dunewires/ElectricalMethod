885551dd0e888b5c62598ad222c0cab3e7431455
nfelt <felt@fas.harvard.edu>
2020-12-22 17:28:56 -0500

    added clock xci

---
+++ firmware/source/cores/ipMicrozed/clk_dwa_pl/clk_dwa_pl_stub.v
@@ -0,0 +1,25 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
+// Date        : Thu Nov 12 17:05:52 2020
+// Host        : lt2 running 64-bit CentOS Linux release 7.6.1810 (Core)
+// Command     : write_verilog -force -mode synth_stub
+//               /home/nate/projects/duneWireTension/vivadoProjects/project_2/project_2.srcs/sources_1/ip/clk_dwa_pl/clk_dwa_pl_stub.v
+// Design      : clk_dwa_pl
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-2
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module clk_dwa_pl(clk_out1, clk_out2, clk_out3, reset, locked, 
+  clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,clk_out2,clk_out3,reset,locked,clk_in1" */;
+  output clk_out1;
+  output clk_out2;
+  output clk_out3;
+  input reset;
+  output locked;
+  input clk_in1;
+endmodule
