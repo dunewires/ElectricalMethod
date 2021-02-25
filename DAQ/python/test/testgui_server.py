aa30646ed52cd8d6938eca650641f83ffb54cfb5
James Battat <jbattat@wellesley.edu>
2021-02-25 15:28:27 -0500

    updated udp server

--- DAQ/python/test/testgui_server.py
+++ DAQ/python/test/testgui_server.py
@@ -7,6 +7,7 @@ import struct
 import math
 import binascii
 import random
+import os
 
 localIP = "127.0.0.1"
 #localPort = 20001
@@ -21,7 +22,7 @@ sock = socket.socket(family=socket.AF_INET,  type=socket.SOCK_DGRAM)
 
 # Bind to address and ip
 #sock.bind((localIP, localPort))
-print("UDP server up and listening")
+print("UDP server initialized")
 
 # Pause until client requests data start
 waitForTrigger = False
@@ -179,7 +180,7 @@ def sendDummyDataDwaMultiregister(sock):
         if len(registers_with_data) == 0:
             break
 
-def sendDummyDataNewHeaders(sock):
+def sendDummyDataNewHeadersOLD(sock):
     print("Reading data from the following files: ")
     #fname= 'newdata_run.dat'
     #filenames = ['newdata_run.dat',
@@ -224,9 +225,80 @@ def sendDummyDataNewHeaders(sock):
         sock.sendto(bytesToSend, address)
         time.sleep(0.005)  # needed otherwise packets are dropped.  not sure how small the sleep can be... 5ms seems to work fine though
 
+
+
+def sendDummyDataNewHeaders(sock):
+    """ Replay data in the nominal format. No need to break apart files 
+    e.g. if fileroot = data/fromSebastien_20210125T010957/'
+    and scanId = 20210125T010957
+    then this code will pull and replay all data from the following files:
+     data/fromSebastien_20210125T010957/20210125T010957_FF.txt
+     data/fromSebastien_20210125T010957/20210125T010957_00.txt
+     data/fromSebastien_20210125T010957/20210125T010957_01.txt
+     ...
+     data/fromSebastien_20210125T010957/20210125T010957_07.txt
+    """
+    #fileroot = 'data/fromSebastien/20200813T000904'
+    #fileroot = 'data/fromSebastien_20210125T010957/20210125T010957'
+    #fileroot = 'data/fromSebastien_20010101/'
+    #scanId = '20210125T010957'
+
+    #fileroot = 'data/fromSebastien_quickScan/'
+    #scanId = '20210224T212726'
+    fileroot = 'data/fromSebastien_slowScan/'
+    scanId = '20210224T205700'
+
+    nChan = 8
+    wireDataFilenames = [ f'{scanId}_{nn:02d}.txt' for nn in range(nChan) ]
+    wireDataFilenames = [ os.path.join(fileroot, ff) for ff in wireDataFilenames ]
+    runHeaderFile = os.path.join(fileroot, f'{scanId}_FF.txt')
+
+    print("Replaying data from the following files: ")
+    print(f"  runHeaderFile = {runHeaderFile}")
+    for ff in wireDataFilenames:
+        print(f"                  {ff}")
+
+    # Open/parse all files in memory
+    udpData = {}
+    udpData['FF'] = getAllLines(runHeaderFile)
+
+    for chan in range(nChan):
+        # Read the full file into memory
+        lines = getAllLines(wireDataFilenames[chan])
+        # Find where the UDP packet boundaries are in the file (lines starting with 'AAAA0')
+        udpDelimIdxs = []
+        for ii, line in enumerate(lines):
+            if line.startswith(b'AAAA0'):
+                udpDelimIdxs.append(ii)
+        # Make a list of lists. Outer level list is one entry per UDP packet
+        # inner level of list is one entry per line of that UDP packet
+        for ii in range(len(udpDelimIdxs)-1):
+            udpData[chan] = [ lines[udpDelimIdxs[ii]:udpDelimIdxs[ii+1]]
+                              for ii in range(len(udpDelimIdxs)-1) ]
+            udpData[chan].append(lines[udpDelimIdxs[-1]:])
+
+    nFreq = len(udpData[0])
+
+    # Prepare for UDP transmission
+    address = (localIP, localPort)
+
+    # Transmit the run header
+    dataToSend = b''.join(udpData['FF'])
+    bytesToSend = binascii.unhexlify(dataToSend)  # convert string to bytes.  e.g. dataToSend is 'CAFE805E' and bytesToSend is b'\xca\xfe\x80^'
+    sock.sendto(bytesToSend, address)
+    time.sleep(0.005)
+
+    # Loop over frequencies and channels and transmit the ADC data
+    for ifreq in range(nFreq):
+        for ichan in range(nChan):
+            bytesToSend = binascii.unhexlify( b''.join(udpData[ichan][ifreq]) )
+            sock.sendto(bytesToSend, address)
+            time.sleep(0.005)  # pause between channel transmissions on a single freq
+            
+        time.sleep(0.07)  # pause after all channels send a single freq.
+
 #sendDummyDataSine(sock)
 #sendDummyDataDwa(sock)
 #sendDummyDataDwaMultiregister(sock)
 sendDummyDataNewHeaders(sock)
 sock.close()
-
