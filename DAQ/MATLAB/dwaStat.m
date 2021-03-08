4e8579d5438af9b9f8bf89e5941a34fc83bb070f
nfelt <felt@fas.harvard.edu>
2021-03-08 13:25:08 -0500

    matlab DAQ update

--- DAQ/MATLAB/dwaStat.m
+++ DAQ/MATLAB/dwaStat.m
@@ -1,15 +1,10 @@
 t=tcpOpenMmtp
 
-%acStim_nPeriod
-regRead(t,"0000000F")
-%acStimX200_nPeriod
+%ctrlbusy
 regRead(t,"00000010")
-%ctrl busy
-regRead(t,"00000011")
-% constant
-regRead(t,"00000012")
-
-% fifoAutoDC_ff fifoAutoDC_ef
-regRead(t,"0000001B")
+%dateCode
+regRead(t,"00000013")
+% hashCode
+regRead(t,"00000014")
 
 tcpCloseMmtp(t);
