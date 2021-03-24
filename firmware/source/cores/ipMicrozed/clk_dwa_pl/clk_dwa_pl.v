d2f40f308fda57be171dbf94b96e1e52d5eee0df
Nathan <felt@fas.harvard.edu>
2021-03-24 12:00:36 -0400

    updated mmcm to include 400MHz

--- firmware/source/cores/ipMicrozed/clk_dwa_pl/clk_dwa_pl.v
+++ firmware/source/cores/ipMicrozed/clk_dwa_pl/clk_dwa_pl.v
@@ -68,7 +68,7 @@
 
 `timescale 1ps/1ps
 
-(* CORE_GENERATION_INFO = "clk_dwa_pl,clk_wiz_v6_0_4_0_0,{component_name=clk_dwa_pl,use_phase_alignment=true,use_min_o_jitter=false,use_max_i_jitter=false,use_dyn_phase_shift=false,use_inclk_switchover=false,use_dyn_reconfig=false,enable_axi=0,feedback_source=FDBK_AUTO,PRIMITIVE=MMCM,num_out_clk=4,clkin1_period=10.000,clkin2_period=10.000,use_power_down=false,use_reset=true,use_locked=true,use_inclk_stopped=false,feedback_type=SINGLE,CLOCK_MGR_TYPE=NA,manual_override=false}" *)
+(* CORE_GENERATION_INFO = "clk_dwa_pl,clk_wiz_v6_0_6_0_0,{component_name=clk_dwa_pl,use_phase_alignment=true,use_min_o_jitter=false,use_max_i_jitter=false,use_dyn_phase_shift=false,use_inclk_switchover=false,use_dyn_reconfig=false,enable_axi=0,feedback_source=FDBK_AUTO,PRIMITIVE=MMCM,num_out_clk=4,clkin1_period=10.000,clkin2_period=10.000,use_power_down=false,use_reset=true,use_locked=true,use_inclk_stopped=false,feedback_type=SINGLE,CLOCK_MGR_TYPE=NA,manual_override=false}" *)
 
 module clk_dwa_pl 
  (
