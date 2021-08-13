d99529530a82af4622231d62daca077cca9abc39
chrisstanford <chris.j.stanford+github@gmail.com>
2021-08-13 14:57:11 -0400

    Submit tensions functionality

--- DAQ/python/database/database_functions.py
+++ DAQ/python/database/database_functions.py
@@ -16,54 +16,63 @@ def get_measured_resonances(layer_data, wire_layer, channel, thresh = 1000.):
             if r not in non_duplicates: non_duplicates.append(r)
     return non_duplicates
 
-def get_layer_data(sietch, tension_frame_uuid, side, wire_layer):
+def get_pointer_table(sietch, apa_uuid, stage):
     '''
-    Gets pointer table associated with the given tension frame, then loops over all the pointers for the given 
-    side and layer and returns a dictionary with the resonances.
+    Obtain most recent pointer table entry.
     '''
+    tension_frame_uuid = get_tension_frame_uuid_from_apa_uuid(sietch, apa_uuid)
     mostRecentPointerTableLookup = ""
     try:
         mostRecentPointerTableLookup = sietch.api("/search/test?limit=1", {
             "formId": "wire_tension_pointer", 
             "componentUuid": tension_frame_uuid,
+            "stage": stage,
             # "layer": layer
         })
     except:
         # No pointer table found for UUID
         # TODO: Make popup message
         return
-    
 
     if mostRecentPointerTableLookup:
         mostRecentPointerTableDBid = mostRecentPointerTableLookup[0]["_id"]
         # Get table from database
         pointerTable = sietch.api("/test/"+mostRecentPointerTableDBid)
-        # Lookup table found, loop over layers
-        wirePointersAllLayers = pointerTable["data"]["wires"]
-        
-        if wire_layer not in wirePointersAllLayers.keys():
-            # TODO: Make popup message
-            print("No pointer table found for layer "+wire_layer+".")
-            return
-        
-        wirePointersForLayer = wirePointersAllLayers[wire_layer]   
-         
-        # Separate the A and B sides
-        pointers = wirePointersForLayer[side]
+        return pointerTable
 
-        # Get list of database ids for the individual wire measurements
-        recordIds = [x["testId"] for x in pointers]
+def get_layer_data(sietch, apa_uuid, side, wire_layer, stage):
+    '''
+    Gets pointer table associated with the given tension frame, then loops over all the pointers for the given 
+    side and layer and returns a dictionary with the resonances.
+    '''
+    # Get table from database
+    pointerTable = get_pointer_table(sietch, apa_uuid, stage)
+    # Lookup table found, loop over layers
+    wirePointersAllLayers = pointerTable["data"]["wires"]
+    
+    if wire_layer not in wirePointersAllLayers.keys():
+        # TODO: Make popup message
+        print("No pointer table found for layer "+wire_layer+".")
+        return
+    
+    wirePointersForLayer = wirePointersAllLayers[wire_layer]   
         
-        # Get database entries for the individual wire resonance measurements
-        resonanceEntries = sietch.api("/test/getBulk",recordIds)
-        
-        # Extract the list of resonances from the database entries
-        resonanceDictionaries = [entry["data"]["wires"] for entry in resonanceEntries]
-        layer_data = {}
-        for d in resonanceDictionaries:
-            for w in d.keys():
-                layer_data[w] = d[w]
-        return layer_data
+    # Separate the A and B sides
+    pointers = wirePointersForLayer[side]
+
+    # Get list of database ids for the individual wire measurements
+    recordIds = [x["testId"] for x in pointers]
+    
+    # Get database entries for the individual wire resonance measurements
+    resonanceEntries = sietch.api("/test/getBulk",recordIds)
+    
+    # Extract the list of resonances from the database entries
+    resonanceDictionaries = [entry["data"]["wires"] for entry in resonanceEntries]
+    layer_data = {}
+    for d in resonanceDictionaries:
+        for w in d.keys():
+            layer_data[w] = d[w]
+    return layer_data
 
 def get_tension_frame_uuid_from_apa_uuid(sietch, apa_uuid):
     '''
