029a2ad0fbeea5164b2855bbc25f04edd78c9256
Nathan <felt@fas.harvard.edu>
2021-09-09 17:27:44 -0400

    updated controller noise settings

--- firmware/source/hdl/mainsNoiseCorrection.vhd
+++ firmware/source/hdl/mainsNoiseCorrection.vhd
@@ -6,7 +6,7 @@
 -- Author      : User Name <user.email@user.company.com>
 -- Company     : User Company Name
 -- Created     : Thu Sep 24 17:35:18 2020
--- Last update : Thu Sep  9 07:12:21 2021
+-- Last update : Thu Sep  9 17:27:11 2021
 -- Platform    : Default Part Number
 -- Standard    : <VHDL-2008 | VHDL-2002 | VHDL-1993 | VHDL-1987>
 --------------------------------------------------------------------------------
@@ -35,7 +35,7 @@ entity mainsNoiseCorrection is
         -- records
         fromDaqReg : in  fromDaqRegType;
         toDaqReg   : out toDaqRegType;
-        freqSet    : in  unsigned(23 downto 0); -- current period (10ns)
+        freqSet    : in  unsigned(23 downto 0);
 
         noiseReadoutBusy  : in boolean := false;
         noiseFirstReadout : in boolean := false;
@@ -81,12 +81,12 @@ architecture struct of mainsNoiseCorrection is
     signal noiseDataEn                            : boolean               := false;
     signal noiseReadoutBusy_del                   : boolean               := false;
     signal resetNoiseData                         : boolean               := false;
-    signal freqInRange                            : boolean               := false;
+    signal mnsEnable                            : boolean               := false;
     signal mem_rstb, mem_rsta_busy, mem_rstb_busy : std_logic             := '0';
 
-    signal senseWireAccDataStrb : std_logic := '0';
-    signal senseWireAccData     : signed_vector_type(7 downto 0)(17 downto 0);
-    signal noiseData            : slv_vector_type(7 downto 0)(17 downto 0);
+    signal memDinStrb : std_logic := '0';
+    signal memDin     : signed_vector_type(7 downto 0)(17 downto 0);
+    signal memDout            : slv_vector_type(7 downto 0)(17 downto 0);
 
     signal mem_resetABusy, mem_resetBBusy : std_logic_vector(7 downto 0);
 
@@ -97,7 +97,7 @@ begin
     begin
         if rising_edge(dwaClk100) then
             if adcStart then
-            elsif senseWireAccDataStrb then --memory write enable
+            elsif memDinStrb then --memory write enable
             end if;
         end if;
     end process;
@@ -105,10 +105,12 @@ begin
     mnsState_seq : process (dwaClk100)
     begin
         if rising_edge(dwaClk100) then
-                        senseWireMNSDataStrb <= '0' ;
-
-            freqInRange <= (freqSetOffset >= fromDaqReg.noiseFreqMin) and
-                (freqSetOffset < fromDaqReg.noiseFreqMax);
+        --default
+            senseWireMNSDataStrb <= '0' ;
+            memWea <= '0';
+            mnsEnable <= (freqSetOffset >= fromDaqReg.noiseFreqMin) and
+                (freqSetOffset < fromDaqReg.noiseFreqMax) and 
+                 not fromDaqReg.useAcStimTrig;
 
             if fromDaqReg.reset then
                 mnsState <= idle_s;
@@ -116,49 +118,70 @@ begin
                 case (mnsState) is
                     when idle_s =>
                         senseWireMNSData(chan_i) <= senseWireData(chan_i)(15 downto 1);
-                        mnsState <= mnsReady_s when freqInRange else idle_s;
+                        mnsState <= mnsReady_s when mnsEnable else idle_s;
 
                     when mnsReady_s =>
+                        -- select output, default mode is to do subtraction, others for debug
                         case (dataSel) is
                             when "00" =>
-                                senseWireMNSData(chan_i) <= senseWireData(chan_i)(15 downto 1);
+                                senseWireMNSData(7 downto 0)(14 downto 0) <= senseWireData(7 downto 0)(15 downto 1);
                             when "01" =>
-                                senseWireMNSData(chan_i) <= senseWireData(chan_i)(15 downto 1) - signed(noiseData(chan_i)(17 downto 3));
+                                senseWireMNSData(7 downto 0)(14 downto 0) <= senseWireData(7 downto 0)(15 downto 1) - signed(memDout(chan_i)(17 downto 3));
                             when "10" =>
-                                senseWireMNSData(chan_i) <= signed(noiseData(chan_i)(17 downto 3));
+                                senseWireMNSData(7 downto 0)(14 downto 0) <= signed(memDout(7 downto 0)(17 downto 3));
                             when "11" =>
-                                senseWireMNSData(chan_i)(14 downto 13) <= "00";
-                                senseWireMNSData(chan_i)(12 downto 8)  <= signed(freqSetOffset(8 DOWNTO 4));
-                                senseWireMNSData(chan_i)(7 downto 0)   <= signed(cnvCnt);
+                                senseWireMNSData(7 downto 0)(14 downto 0) <= (others =>(14 downto 0 => "00" & signed(freqSetOffset(8 DOWNTO 4)) & signed(cnvCnt)));
                             when others =>
-                                null;
                         end case;
-                        if not freqInRange then
+
+                        if not mnsEnable then -- disable mains subtraction
                             mnsState <= idle_s;
-                        else
+                        else  -- mains subtraction is active
                             if adcStart then
+                                -- start of ADC conversion of new frequency 
+                                -- there are ~80 clocks before the conversion is done.
+                                -- either start memory init or get first interpolated noise data in current frequency
                                 cnvCnt   <= (others => '0');
                                 mnsState <= initMem_s when noiseReadoutBusy else getNoise0_s;
                             elsif senseWireDataStrb then
+                                -- ADC conversion done, get interpolated noise data for remaining samples in current frequency
                                 cnvCnt   <= cnvCnt+1;
-                                mnsState <= initMem_s;
+                                mnsState <= getNoise0_s;
                             end if;
                         end if;
 
                     when initMem_s =>
-                        senseWireAccData(chan_i) <= resize(senseWireData(chan_i)(15 downto 1),18);
-                         senseWireAccDataStrb <= senseWireDataStrb;
-                        if senseWireDataStrb then
-                            cnvCnt <= cnvCnt + 1;
-                        end if;
-                        if adcStart then
+
+                        for chan_i in 7 downto 0 loop -- write first samples directly to memory
+                            memDin(chan_i) <= resize(senseWireData(chan_i)(15 downto 1),18);
+                        end loop;
+
+                        if senseWireDataStrb then  -- valid data from ADC
+                            cnvCnt <= cnvCnt + 1;  -- memAddr will be delayed 1 clock to allow current write
+                            memWea <= '1';
+                        elsif adcStart then  -- this is the second adcStart, the remainig samples should be accumulated 
+                            cnvCnt   <= (others => '0');
                             mnsState <= accumMem_s;
-                        elsif not noiseResetBusy then
-                            mnsState <= idle_s;
+                        elsif not noiseReadoutBusy then
+                            mnsState <= mnsReady_s; -- path back to mnsReady_s if we are done collecting noise. eg no averaging.
                         end if;
 
-                        senseWireAccData(chan_i) <= resize(senseWireData(chan_i)(15 downto 1),18) + signed(noiseData(chan_i));
+                    when accumMem_s =>
+                        for chan_i in 7 downto 0 loop -- accumulate samples 
+                            memDin(chan_i) <= resize(senseWireData(chan_i)(15 downto 1),18) + signed(memDout(chan_i));
+                        end loop;
+
+                        if senseWireDataStrb then
+                            cnvCnt <= cnvCnt + 1;
+                            memWea <= '1';
+                         elsif adcStart then -- accumulate another set of samples for current frequency
+                            cnvCnt   <= (others => '0');
+                        elsif not noiseReadoutBusy then
+                            mnsState <= mnsReady_s; -- path back to mnsReady_s if we are done collecting noise.
+                        end if;
+
                     when getNoise0_s =>
+                        memAddr <= 
                         mnsState <= getNoise1_s;
 
                     when getNoise1_s =>
@@ -166,7 +189,6 @@ begin
 
                     when others =>
                         mnsState <= idle_s;
-                        null;
                 end case;
             end if;
         end if;
@@ -179,16 +201,16 @@ begin
             PORT MAP (
                 clka               => dwaClk100,
                 ena                => '1',
-                wea(0)             => senseWireAccDataStrb,
+                wea(0)             => memDinStrb,
                 addra(12 downto 8) => std_logic_vector(freqSet(8 DOWNTO 4)), -- shifting by 4 will give a x16 interpolation
                 addra(7 downto 0)  => std_logic_vector(cnvCnt),
-                dina               => std_logic_vector(senseWireAccData(chan_i)),
+                dina               => std_logic_vector(memDin(chan_i)),
                 clkb               => dwaClk100,
                 rstb               => mem_rstb,
                 enb                => '1',
                 addrb(12 downto 8) => std_logic_vector(freqSetOffset(8 DOWNTO 4)), -- shifting by 4 will give a x16 interpolation
                 addrb(7 downto 0)  => std_logic_vector(cnvCnt),
-                doutb              => noiseData(chan_i),
+                doutb              => memDout(chan_i),
                 rsta_busy          => mem_resetABusy(chan_i),
                 rstb_busy          => mem_resetBBusy(chan_i)
             );
@@ -208,11 +230,11 @@ begin
                 if noiseReadoutBusy then
                 else
                     senseWireMNSDataStrb <= senseWireDataStrb;
-                    senseWireAccDataStrb <= '0' ;
+                    memDinStrb <= '0' ;
                 end if;
                 -- divide the x8 accumulated noise samples by 8 and subtract from sense wire samples.
                 -- outside the range just pass along the samples
-                if freqInRange then
+                if mnsEnable then
                 else
                 end if;
 
@@ -223,13 +245,13 @@ begin
     end generate genChMem;
 
     --   senseWireMNSData(0) <= senseWireData(0)(15 downto 1);
-    --   senseWireMNSData(1) <= signed(noiseData(0)(17 downto 3));
+    --   senseWireMNSData(1) <= signed(memDout(0)(17 downto 3));
     --   senseWireMNSData(2) <= senseWireData(1)(15 downto 1);
-    --   senseWireMNSData(3) <= signed(noiseData(1)(17 downto 3));
+    --   senseWireMNSData(3) <= signed(memDout(1)(17 downto 3));
     --   senseWireMNSData(4) <= senseWireData(2)(15 downto 1);
-    --   senseWireMNSData(5) <= signed(noiseData(2)(17 downto 3));
+    --   senseWireMNSData(5) <= signed(memDout(2)(17 downto 3));
     --   senseWireMNSData(6) <= senseWireData(3)(15 downto 1);
-    --   senseWireMNSData(7) <= signed(noiseData(3)(17 downto 3));
+    --   senseWireMNSData(7) <= signed(memDout(3)(17 downto 3));
 
     -- reset at the beginning of the noise recording.
     mem_rstb  <= '0';   --bool2sl(noiseReadoutBusy and not noiseReadoutBusy_del);
--- firmware/source/hdl/wtaController.vhd
+++ firmware/source/hdl/wtaController.vhd
@@ -6,7 +6,7 @@
 -- Author      : User Name <user.email@user.company.com>
 -- Company     : User Company Name
 -- Created     : Thu May  2 11:04:21 2019
--- Last update : Thu Aug 12 14:05:18 2021
+-- Last update : Thu Sep  9 16:25:15 2021
 -- Platform    : Default Part Number
 -- Standard    : <VHDL-2008 | VHDL-2002 | VHDL-1993 | VHDL-1987>
 --------------------------------------------------------------------------------
@@ -36,9 +36,8 @@ entity wtaController is
 		freqSet       : out unsigned(23 downto 0) := (others => '1');
 		acStim_enable : out std_logic             := '0';
 
-		noiseReadoutBusy  : out boolean := false;
-		noiseFirstReadout : out boolean := false;
-		noiseResetBusy    : in  boolean := false;
+		noiseReadoutBusy : out boolean := false;
+		noiseResetBusy   : in  boolean := false;
 
 		sendRunHdr  : out boolean := false;
 		sendAdcData : out boolean := false;
@@ -72,157 +71,159 @@ begin
 	ctrlState_seq : process (dwaClk100)
 	begin
 		if rising_edge(dwaClk100) then
-			scanAbort         <= (scanAbort or fromDaqReg.scanAbort) and freqScanBusy;
-			scanDone          <= (freqSet >= fromDaqReg.noiseFreqMax) when noiseReadoutBusy else (freqSet >= fromDaqReg.stimFreqMax);
-			toDaqReg.ctrlBusy <= true;
-			ctrlStart_del     <= fromDaqReg.ctrlStart;
-			adcBusy_del       <= adcBusy;
-			adcStart          <= false;
-			sendRunHdr        <= false;
-			sendAdcData       <= false;
-			noiseFirstReadout <= noiseReadoutCnt = x"1";
-			pktBuildBusy_del  <= pktBuildBusy;
-			--generate pulse when we finish a task
-			pktBuildDone <= not pktBuildBusy and pktBuildBusy_del;
+			scanAbort <= (scanAbort or fromDaqReg.scanAbort) and freqScanBusy;
+			--the noise scan will 
+			scanDone <= (freqSet >= fromDaqReg.noiseFreqMax) when noiseReadoutBusy else (freqSet >= (fromDaqReg.stimFreqMax + fromDaqReg.noiseFreqStep);
+					toDaqReg.ctrlBusy <= true;
+					ctrlStart_del     <= fromDaqReg.ctrlStart;
+					adcBusy_del       <= adcBusy;
+					adcStart          <= false;
+					sendRunHdr        <= false;
+					sendAdcData       <= false;
+					pktBuildBusy_del  <= pktBuildBusy;
+					--generate pulse when we finish a task
+					pktBuildDone <= not pktBuildBusy and pktBuildBusy_del;
 
-			if fromDaqReg.reset then
-				ctrlState <= idle_s;
-			else
-
-				case (ctrlState) is
-
-					when idle_s => --test is done and set freq to the beginning
-						toDaqReg.ctrlBusy <= false;
-						noiseReadoutCnt   <= x"1";
-						noiseReadoutBusy  <= false;
-						freqScanBusy      <= false;
-						timerCnt          <= x"00000000";
-						--turn off stimulus 
-						acStim_enable <= '0';
-
-						if fromDaqReg.ctrlStart and not ctrlStart_del then
-							sendRunHdr   <= true; --send run header at start of test.
-							freqScanBusy <= true;
-							-- if we are triggering on the stimulus, the noise subtraction won't work
-							-- !! change this to mains noise disable and fix in MNS instance. 
-							if fromDaqReg.useAcStimTrig then
-								freqSet   <= fromDaqReg.stimFreqMin;
-								ctrlState <= stimEnable_s;
-							else
-								noiseReadoutBusy <= true; -- only count 
-								freqSet          <= fromDaqReg.noiseFreqMin;
-								ctrlState        <= noisePrep_s;
-							end if;
-						end if;
-
-					when noisePrep_s =>
-						if scanAbort then --abort readout and send end of run header
-							noiseReadoutBusy <= false;
-							freqScanBusy     <= false;
-							timerCnt         <= x"00000000";
-							ctrlState        <= freqScanFinish_s;
-						else
-							--wait a bit for the division to update and BPF to respond to the new setting
-							if timerCnt(31 downto 8) <= fromDaqReg.noiseBPFSetTime then
-								timerCnt <= timerCnt +1;  -- only count 
-							                              -- check FIFOs are not almost full
-							elsif not noiseResetBusy then -- wait for the noise memory to be reset
-								adcStart  <= true;
-								timerCnt  <= x"00000000";
-								ctrlState <= noiseReadout_s;
-							end if;
-						end if;
-
-					when noiseReadout_s => --wait a bit for the division to update and check FIFOs are not almost full
-						                   -- wait for falling edge of adcBusy;
-						if adcBusy = '0' and adcBusy_del = '1' then
-							if scanDone then
-								if noiseReadoutCnt = x"8" then --we are done with the noise
-									freqSet          <= fromDaqReg.stimFreqMin;
-									noiseReadoutBusy <= false;
-									ctrlState        <= stimEnable_s; -- start stimulus 
-								else                                  -- accumulate more noise samples to average 
-									freqSet         <= fromDaqReg.noiseFreqMin;
-									noiseReadoutCnt <= noiseReadoutCnt +1;
-									ctrlState       <= noisePrep_s;
-								end if;
-							else
-								freqSet   <= freqSet+fromDaqReg.noiseFreqStep;
-								ctrlState <= noisePrep_s;
-							end if;
-						end if;
-
-					when stimEnable_s =>      -- when the stim is first enabled, wait a bit
-						acStim_enable <= '1'; -- Turn on stimulus 
-						timerCnt      <= timerCnt+1;
-						if timerCnt(31 downto 8) >= fromDaqReg.stimTimeInitial then
-							timerCnt  <= x"00000000";
-							ctrlState <= stimPrep_s;
-						end if;
-
-					when stimPrep_s =>    --wait a bit for the divison to update and check FIFOs are not almost full
-						if scanAbort then --abort readout and send end of run header
-							freqScanBusy <= false;
-							timerCnt     <= x"00000000";
-							ctrlState    <= freqScanFinish_s;
-						else
-							if timerCnt <= x"00000020" then                     --give at least 32 clocks for division to happen
-								timerCnt <= timerCnt +1;                        -- only count 
-							elsif (adcAutoDc_af = x"00") and adcBusy = '0' then -- check fifos and adc is not in a readout
-								timerCnt  <= x"00000000";
-								ctrlState <= stimRun_s;
-							end if;
-						end if;
-
-					when stimRun_s => -- count the number of clock cycles we stim before ADC readout
-						if timerCnt(31 downto 8) >= fromDaqReg.stimTime then
-							if not pktBuildBusy then -- don't start collecting new data until pktBuilder is finished 
-								timerCnt  <= x"00000000";
-								ctrlState <= stimReadout_s;
-								adcStart  <= true;
-							end if;
-						else
-							timerCnt <= timerCnt+1;
-						end if;
-
-					when stimReadout_s => --sending of ADC data packet
-						                  -- wait for falling edge of adcBusy;
-						if adcBusy = '0' and adcBusy_del = '1' then
-							sendAdcData <= true;
-							if scanDone then
-								ctrlState    <= freqScanFinish_s;
-								freqScanBusy <= false;
-							else
-								freqSet   <= freqSet+fromDaqReg.stimFreqStep;
-								ctrlState <= stimPrep_s;
-							end if;
-						end if;
-
-					-- the packet builder may or may not have just received a start command when we get the abort command
-					when freqScanFinish_s => --wait for packet builder busy to respond then wait 'till its done
-						if timerCnt >= x"00000008" then
-							if not pktBuildBusy then
-								sendRunHdr <= true; --send run header at end of test.
-								timerCnt   <= x"00000000";
-								ctrlState  <= pktBuildFinish_s;
-							end if;
-						else
-							timerCnt <= timerCnt+1;
-						end if;
-
-					when pktBuildFinish_s => --wait for packet to be sent before allowing another test 
-						if pktBuildDone then -- We will always see the packet builder finish in this case
-							ctrlState <= idle_s;
-						end if;
-
-					when others =>
+					if fromDaqReg.reset then
 						ctrlState <= idle_s;
-						null;
-				end case;
-			end if;
-		end if;
-	end process ctrlState_seq;
+					else
 
-	toDaqReg.ctrlStateDbg <= to_unsigned(ctrlState_type'POS(ctrlState),toDaqReg.ctrlStateDbg'length);
+						case (ctrlState) is
 
-end architecture rtl;
+							when idle_s => --test is done and set freq to the beginning
+								toDaqReg.ctrlBusy <= false;
+								noiseReadoutCnt   <= x"1";
+								noiseReadoutBusy  <= false;
+								freqScanBusy      <= false;
+								timerCnt          <= x"00000000";
+								--turn off stimulus 
+								acStim_enable <= '0';
+
+								if fromDaqReg.ctrlStart and not ctrlStart_del then
+									sendRunHdr   <= true; --send run header at start of test.
+									freqScanBusy <= true;
+									-- if we are triggering on the stimulus, the noise subtraction won't work
+									-- !! change this to mains noise disable and fix in MNS instance. 
+									if fromDaqReg.useAcStimTrig then
+										freqSet   <= fromDaqReg.stimFreqMin;
+										ctrlState <= stimEnable_s;
+									else
+										freqSet   <= fromDaqReg.noiseFreqMin;
+										ctrlState <= noisePrep_s;
+								end if;
+							end if;
+
+							when noisePrep_s =>
+								if scanAbort then --abort readout and send end of run header
+									noiseReadoutBusy <= false;
+									freqScanBusy     <= false;
+									timerCnt         <= x"00000000";
+									ctrlState        <= freqScanFinish_s;
+								else
+									noiseReadoutBusy <= true; -- tell the mainsNoiseCorrection that we care collecting noise samples
+									                          --wait a bit for the division to update and BPF to respond to the new setting
+									if timerCnt(31 downto 8) <= fromDaqReg.noiseBPFSetTime then
+										timerCnt <= timerCnt +1;  -- only count 
+									                              -- check FIFOs are not almost full
+									elsif not noiseResetBusy then -- wait for the noise memory to be reset
+										adcStart  <= true;
+										timerCnt  <= x"00000000";
+										ctrlState <= noiseReadout_s;
+								end if;
+							end if;
+
+							when noiseReadout_s =>
+								if adcBusy = '0' and adcBusy_del = '1' then -- wait for falling edge of adcBusy;
+									if noiseReadoutCnt < x"8" then
+										-- for each frequency, sample noise 8 times
+										-- As soon as the ADC is finished with a series of samples
+										-- adcStart will initiate the trigger on mains frequency
+										adcStart        <= true;
+										noiseReadoutCnt <= noiseReadoutCnt +1;
+									else                           -- we are done averaging samples for the current freq
+										noiseReadoutBusy <= false; -- tell the mainsNoise Correction we are finished collecting noise samples for current frequency
+										noiseReadoutCnt  <= x"1";
+										if not scanDone then -- we have collected all samples in range +1
+											freqSet   <= freqSet+fromDaqReg.noiseFreqStep; -- on to the next frequency's noise samples
+											ctrlState <= noisePrep_s;
+										else
+											freqSet   <= fromDaqReg.stimFreqMin;
+											ctrlState <= stimEnable_s; -- start stimulus 
+									end if;
+								end if;
+							end if;
+
+							when stimEnable_s =>      -- when the stim is first enabled, wait a bit
+								acStim_enable <= '1'; -- Turn on stimulus 
+								timerCnt      <= timerCnt+1;
+								if timerCnt(31 downto 8) >= fromDaqReg.stimTimeInitial then
+									timerCnt  <= x"00000000";
+									ctrlState <= stimPrep_s;
+							end if;
+
+							when stimPrep_s =>    --wait a bit for the divison to update and check FIFOs are not almost full
+								if scanAbort then --abort readout and send end of run header
+									freqScanBusy <= false;
+									timerCnt     <= x"00000000";
+									ctrlState    <= freqScanFinish_s;
+								else
+									if timerCnt <= x"00000020" then                     --give at least 32 clocks for division to happen
+										timerCnt <= timerCnt +1;                        -- only count 
+									elsif (adcAutoDc_af = x"00") and adcBusy = '0' then -- check fifos and adc is not in a readout
+										timerCnt  <= x"00000000";
+										ctrlState <= stimRun_s;
+								end if;
+							end if;
+
+							when stimRun_s => -- count the number of clock cycles we stim before ADC readout
+								if timerCnt(31 downto 8) >= fromDaqReg.stimTime then
+									if not pktBuildBusy then -- don't start collecting new data until pktBuilder is finished 
+										timerCnt  <= x"00000000";
+										ctrlState <= stimReadout_s;
+										adcStart  <= true;
+								end if;
+								else
+									timerCnt <= timerCnt+1;
+							end if;
+
+							when stimReadout_s => --sending of ADC data packet
+								                  -- wait for falling edge of adcBusy;
+								if adcBusy = '0' and adcBusy_del = '1' then
+									sendAdcData <= true;
+									if scanDone then
+										ctrlState    <= freqScanFinish_s;
+										freqScanBusy <= false;
+									else
+										freqSet   <= freqSet+fromDaqReg.stimFreqStep;
+										ctrlState <= stimPrep_s;
+								end if;
+							end if;
+
+							-- the packet builder may or may not have just received a start command when we get the abort command
+							when freqScanFinish_s => --wait for packet builder busy to respond then wait 'till its done
+								if timerCnt >= x"00000008" then
+									if not pktBuildBusy then
+										sendRunHdr <= true; --send run header at end of test.
+										timerCnt   <= x"00000000";
+										ctrlState  <= pktBuildFinish_s;
+								end if;
+								else
+									timerCnt <= timerCnt+1;
+							end if;
+
+							when pktBuildFinish_s => --wait for packet to be sent before allowing another test 
+								if pktBuildDone then -- We will always see the packet builder finish in this case
+									ctrlState <= idle_s;
+							end if;
+
+							when others =>
+								ctrlState <= idle_s;
+								null;
+					end case;
+				end if;
+					end if;
+					end process ctrlState_seq;
+
+					toDaqReg.ctrlStateDbg <= to_unsigned(ctrlState_type'POS(ctrlState),toDaqReg.ctrlStateDbg'length);
+
+					end architecture rtl;
