c57c8c61140b194c1a8c83c13ff7601c5baff3a2
Nathan <felt@fas.harvard.edu>
2021-11-02 12:15:37 -0400

    added range comments

--- firmware/source/packages/global_def.vhd
+++ firmware/source/packages/global_def.vhd
@@ -6,7 +6,7 @@
 -- Author      : Nathan Felt felt@fas.harvard.edu
 -- Company     : Harvard University LPPC
 -- Created     : Thu May  2 11:04:21 2019
--- Last update : Thu Oct  7 17:26:26 2021
+-- Last update : Thu Oct 28 15:02:20 2021
 -- Platform    : DWA microZed
 -- Standard    : VHDL-2008
 -------------------------------------------------------------------------------
@@ -63,6 +63,7 @@ package global_def is
         reset     : boolean;
         scanAbort : boolean;
 
+
         auto        : boolean;
         mnsEna      : boolean;
         udpDataDone : boolean;
@@ -71,63 +72,62 @@ package global_def is
 
         -- start james' additions
         --- dwaCtrl (still used?  how many bits?)
-        fixedPeriod     : unsigned(23 downto 0); -- 10ns
-        stimFreqReq     : unsigned(23 downto 0); --nf
-        stimFreqMin     : unsigned(23 downto 0); -- 10ns
-        stimFreqMax     : unsigned(23 downto 0); -- 10ns
-        stimFreqStep    : unsigned(23 downto 0); -- 10ns
-        noiseFreqMin    : unsigned(23 downto 0); -- 40 Hz
-        noiseFreqMax    : unsigned(23 downto 0); -- 70 Hz
-        noiseFreqStep   : unsigned(23 downto 0); -- 1 Hz
-        noiseSampPer    : unsigned(23 downto 0); -- 32 samp / cycle @ 60 Hz
-        noiseNCnv       : unsigned(23 downto 0);
-        noiseBPFSetTime : unsigned(23 downto 0);
+        fixedPeriod     : unsigned(23 downto 0); 
+        stimFreqReq     : unsigned(23 downto 0); --10 Hz  , 1000 Hz
+        stimFreqMin     : unsigned(23 downto 0); --10 Hz  , max
+        stimFreqMax     : unsigned(23 downto 0); -- min , 1000 Hz
+        stimFreqStep    : unsigned(23 downto 0); -- 1  ,  (freqMax - freqMin)
+        noiseFreqMin    : unsigned(23 downto 0); -- stimFreqMin , noiseFreMax when MNS enabled
+        noiseFreqMax    : unsigned(23 downto 0); -- ((noiseFreqMin - stimFreqMax)/step <= 32) , (noiseFreqMin - stimFreqMax) when MNS enabled
+        noiseFreqStep   : unsigned(23 downto 0); -- TBD - noiseFreqMax - NoiseFreqMin, when MNS enabled
+        noiseSampPer    : unsigned(23 downto 0); -- -- unused !!remove from firmware / Daq
+        noiseNCnv       : unsigned(23 downto 0);    -- unused !!remove from firmware / Daq
+        noiseBPFSetTime : unsigned(23 downto 0);  -- anything
 
-        serNum           : unsigned(23 downto 0);
-        serNumMemAddress : unsigned(12 downto 0);
-        serNumMemData    : unsigned(31 downto 0);
+        serNum           : unsigned(23 downto 0); -- not from register
+        serNumMemAddress : unsigned(12 downto 0);  -- anything
+        serNumMemData    : unsigned(31 downto 0);-- anything
 
-        serNumMemRead     : std_logic;
-        serNumMemAddrStrb : std_logic;
-        serNumMemWrite    : std_logic;
+        serNumMemRead     : std_logic;-- anything
+        serNumMemAddrStrb : std_logic;-- anything
+        serNumMemWrite    : std_logic;-- anything
         --stimPeriodActive   : unsigned(23 downto 0);
         --stimPeriodCounter  : unsigned(23 downto 0); -- bits???
         --- Number of stimulus cycles per frequency (unitless)
-        cyclesPerFreq : unsigned(23 downto 0);      -- bits???
+        cyclesPerFreq : unsigned(23 downto 0);      -- fromDaqReg.cyclesPerFreq * fromDaqReg.adcSamplesPerCycle < 4096 
                                                     --- Number of ADC samples per simulus cycle (unitless)
-        adcSamplesPerCycle : unsigned(15 downto 0); -- bits???
+        adcSamplesPerCycle : unsigned(15 downto 0); -- fromDaqReg.cyclesPerFreq * fromDaqReg.adcSamplesPerCycle < 4096
                                                     --- AC Stimulus magnitude (12bit DAC value)
-        stimMag       : unsigned(11 downto 0);
-        senseWireGain : SLV_VECTOR_TYPE(7 downto 0)(7 downto 0);
+        stimMag       : unsigned(11 downto 0);  -- any value
+        senseWireGain : SLV_VECTOR_TYPE(7 downto 0)(7 downto 0);-- anyvalue
         dateCode      : std_logic_vector(47 downto 0);
         hashCode      : std_logic_vector(31 downto 0);
         -- Client IP address (where UDP data is sent)
-        clientIp : unsigned(31 downto 0);
+        clientIp : unsigned(31 downto 0); -- anyvalue
         --- After switching to a new frequency, how long to wait before
         --- acquiring data (24bits, units=1.28 microseconds)
-        stimTime : unsigned(23 downto 0);
+        stimTime : unsigned(23 downto 0); -- anyvalue
         -- extra time to wait after initially enabling the stimulus frequency at the start of a run
-        stimTimeInitial : unsigned(23 downto 0);
+        stimTimeInitial : unsigned(23 downto 0);-- anyvalue
 
         ctrlStateDbg         : unsigned(3 downto 0);
         errors               : std_logic_vector(23 downto 0);
-        statusPeriod         : unsigned(23 downto 0);
-        pktGenWatchdogPeriod : unsigned(23 downto 0);
+        statusPeriod         : unsigned(23 downto 0); --anyvalue
+        pktGenWatchdogPeriod : unsigned(23 downto 0); --anyvalue
         --- Channel mask indicating which sense channels are active (8bit)
-        activeChannels : std_logic_vector(7 downto 0);
+        activeChannels : std_logic_vector(7 downto 0);  -- !! unused, remove from firmware.
         --- Mask indicating which relays are active
         --- in v2 this is 32 bits.  In v3 will be 192 bits!!!
-        relayMask : std_logic_vector(31 downto 0);
+        relayMask : std_logic_vector(31 downto 0); --!! unused remove from firmware.
         --adcSamplingPeriod  : unsigned(23 downto 0);
-        relayBusTop       : SLV_VECTOR_TYPE(1 downto 0)(15 downto 0);
-        relayWireTop      : SLV_VECTOR_TYPE(3 downto 0)(15 downto 0);
-        relayBusBot       : SLV_VECTOR_TYPE(1 downto 0)(15 downto 0);
-        relayWireBot      : SLV_VECTOR_TYPE(3 downto 0)(15 downto 0);
-        relayUpdate       : boolean;
-        relayAutoBreakEna : std_logic;
-        useAcStimTrig     : std_logic;
-
         netStatus : std_logic_vector(7 downto 0);
+        relayBusTop       : SLV_VECTOR_TYPE(1 downto 0)(15 downto 0);--TBD
+        relayWireTop      : SLV_VECTOR_TYPE(3 downto 0)(15 downto 0);--TBD
+        relayBusBot       : SLV_VECTOR_TYPE(1 downto 0)(15 downto 0);--TBD
+        relayWireBot      : SLV_VECTOR_TYPE(3 downto 0)(15 downto 0);--TBD
+        relayUpdate       : boolean;--!! mask when realy registers are not correctly comfigured.
+        relayAutoBreakEna : std_logic; --anything
+        useAcStimTrig     : std_logic; -- anything
     end record; -- fromDaqRegType
 
 end global_def;
