2701c883b99b9fc86dfb1b95af1c41f71324ee6d
Sebastien Prince <sebas.prince@icloud.com>
2022-02-09 14:57:32 -0500

    Add DWA hardware test

--- DAQ/python/dwaInit.py
+++ DAQ/python/dwaInit.py
@@ -1,7 +1,65 @@
-#%%
 import DwaMicrozed as duz
 from contextlib import redirect_stdout
 from os import devnull
+
+print('Beginning of DWA hardware test.')
+
+digipots = 'FFDDBB9977553311'
+stimMag = '012'
+relays = {}
+relays['relayWireBot0'] = '00001111'
+relays['relayWireBot1'] = '00002222'
+relays['relayWireBot2'] = '00003333'
+relays['relayWireBot3'] = '00004444'
+relays['relayBusBot0'] = '00005555'
+relays['relayBusBot1'] = '00006666'
+relays['relayWireTop0'] = '00007777'
+relays['relayWireTop1'] = '00008888'
+relays['relayWireTop2'] = '00009999'
+relays['relayWireTop3'] = '0000AAAA'
+relays['relayBusTop0'] = '0000BBBB'
+relays['relayBusTop1'] = '0000CCCC'
+
+print('Writing values for digipots, AC amplitude and relays to firmware...')
+with redirect_stdout(devnull_file):
+    # Use default IP address, triggered by hardware jumper between pins 0 and 1 on 2x4 header
+    uz = duz.DwaMicrozed(ip='140.247.123.186')
+    uz.setDigipots(digipots)
+    uz.setStimMag(stimMag)
+    uz.setRelays(relays)
+print('Done.')
+
+print('Reading back values for digipots, AC amplitude and relays from firmware...')
+with redirect_stdout(devnull_file):
+    digipots_read_low = format(uz.readValue('0000000F')[2], '08X')
+    digipots_read_high = format(uz.readValue('00000010')[2], '08X')
+    stimMag_read = format(uz.readValue('00000008')[2], '03X')
+    relays_read = {}
+    relays_read['relayWireBot0'] = format(uz.readValue('00000020')[2], '08X')
+    relays_read['relayWireBot1'] = format(uz.readValue('00000021')[2], '08X')
+    relays_read['relayWireBot2'] = format(uz.readValue('00000022')[2], '08X')
+    relays_read['relayWireBot3'] = format(uz.readValue('00000023')[2], '08X')
+    relays_read['relayBusBot0'] = format(uz.readValue('00000024')[2], '08X')
+    relays_read['relayBusBot1'] = format(uz.readValue('00000025')[2], '08X')
+    relays_read['relayWireTop0'] = format(uz.readValue('00000026')[2], '08X')
+    relays_read['relayWireTop1'] = format(uz.readValue('00000027')[2], '08X')
+    relays_read['relayWireTop2'] = format(uz.readValue('00000028')[2], '08X')
+    relays_read['relayWireTop3'] = format(uz.readValue('00000029')[2], '08X')
+    relays_read['relayBusTop0'] = format(uz.readValue('0000002A')[2], '08X')
+    relays_read['relayBusTop1'] = format(uz.readValue('0000002B')[2], '08X')
+print('Done.')
+
+values_written = (digipots, stimMag, relays)
+values_read = (digipots_high + digipots_low, stimMag_read, relays_read)
+if values_written == values_read:
+    print('Values successfully written and read back.')
+else:
+    raise ValueError(f'Values not successfully written and read back: {values_written=} not equal to {values_read=}')
+
+print('End of hardware test.')
+
+
+print('Beginning of DWA initializtion.')
 
 serial_number_int = 0
 
@@ -25,25 +83,30 @@ devnull_file open(devnull, 'w')
 with redirect_stdout(devnull_file):
     # Use default IP address, triggered by hardware jumper between pins 0 and 1 on 2x4 header
     uz = duz.DwaMicrozed(ip='140.247.123.186')
-    uz.initialize_dwa(serial_number, ip_address, mac_address_msb, mac_address_lsb)
+    uz.dwaInitialize(serial_number, ip_address, mac_address_msb, mac_address_lsb)
+print('Done.')
 
-print('Initialized DWA serial number, local IP address and MAC address.')
-print('Validating values from firmware...')
+print('Reading back values for DWA serial number, local IP address and MAC address from firmware...')
 # Read back values and convert
 with redirect_stdout(devnull_file):
-    serial_number_output = uz.readValue('00000032')[2]
-    ip_adress_output = uz.readValue('00000032')[2]
-    mac_adress_msb_output = uz.readValue('00000032')[2]
-    mac_adress_lsb_output = uz.readValue('00000032')[2]
+    serial_number_read = format(uz.readValue('00000032')[2], '08X')
+    ip_adress_read = format(uz.readValue('00000032')[2], '08X')
+    mac_adress_msb_read = format(uz.readValue('00000032')[2], '08X')
+    mac_adress_lsb_read = format(uz.readValue('00000032')[2], '08X')
+print('Done.')
 
-if (serial_number, ip_address, mac_address_msb, mac_address_lsb) == (serial_number_output, ip_address_output, mac_address_msb_output, mac_address_lsb_output):
+values_written = (serial_number, ip_address, mac_address_msb, mac_address_lsb)
+values_read = (serial_number_read, ip_address_read, mac_address_msb_read, mac_address_lsb_read)
+if values_written == values_read:
     ip_address_split = [str(int(ip_address[i:i+2], 16)) for i in range(0, len(ip_address), 2)]
     mac_address = mac_address_msb[2:] + mac_address_lsb[2:]
     mac_address_split = [mac_address[i:i+2] for i in range(0, len(mac_address), 2)]
-    print('Validated values:')
+    print('Values successfully written and read back.')
+    print('Please record the following values for this DWA.')
     print('DWA serial number: ' + serial_number_input)
     print('Local IP address: ' + '.'.join(ip_address_split))
     print('MAC address: ' + ':'.join(mac_address_split))
-
 else:
-    raise ValueError('Read back initialization values do not correspond to written initialization values.')
+    raise ValueError(f'Values not successfully written and read back: {values_written=} not equal to {values_read=}')
+
+print('End of DWA initializtion.')
