00b8626bac76a232149669fc69c24d557338f1bf
Nathan Felt <felt@g.harvard.edu>
2022-02-17 12:38:07 -0500

    updated program script

--- firmware/scripts/implementation/progFlashV2.tcl
+++ firmware/scripts/implementation/progFlashV2.tcl
@@ -10,12 +10,12 @@
 open_hw_manager
 connect_hw_server -url 127.0.0.1:3121 -allow_non_jtag
  ## t #jeff
-    current_hw_target [get_hw_targets */xilinx_tcf/Xilinx/00001c726cd801]
-    set_property PARAM.FREQUENCY 6000000 [get_hw_targets */xilinx_tcf/Xilinx/00001c726cd801]
- ## t #nw
+    #current_hw_target [get_hw_targets */xilinx_tcf/Xilinx/00001c726cd801]
+    #set_property PARAM.FREQUENCY 6000000 [get_hw_targets */xilinx_tcf/Xilinx/00001c726cd801]
+ ## t if there is only one cable
 
-	# current_hw_target [get_hw_targets */xilinx_tcf/Digilent/210249A0D0C2]
-	# set_property PARAM.FREQUENCY 6000000 [get_hw_targets */xilinx_tcf/Digilent/210249A0D0C2]
+	current_hw_target [get_hw_targets *]
+	set_property PARAM.FREQUENCY 6000000 [get_hw_targets *]
 
 open_hw_target
 current_hw_device [get_hw_devices xc7z020_1]
@@ -31,7 +31,7 @@ set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [ge
 set_property PROGRAM.VERIFY  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7z020_1] 0]]
 set_property PROGRAM.CHECKSUM  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7z020_1] 0]]
 program_hw_cfgmem -hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7z020_1] 0]]
-boot_hw_device  [lindex [get_hw_devices xc7z020_1] 0]
+catch {boot_hw_device  [lindex [get_hw_devices xc7z020_1] 0]}
 close_hw_target
 close_hw_manager
 
