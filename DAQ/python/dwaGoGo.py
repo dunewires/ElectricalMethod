dbc038157c851e4a1b6c934b0dbadc431d89fed2
Nathan Felt <felt@fas.harvard.edu>
2024-04-11 17:22:12 -0400

    updated tools to use defaule 192.171.140.7, started dwaGoGo script

--- DAQ/python/dwaGoGo.py
+++ DAQ/python/dwaGoGo.py
@@ -1,14 +1,35 @@
 import dwaTools as dwa
+import time
 
-print('\n\n======= dwaReset() ===========')
-dwa.dwaReset(verbose=1)
+print('\n\n======= dwaRelayConfigTest() ===========')
+#dwa.dwaReset(verbose=1)
+sleepSec = 0.2
 
-print('\n\n======= dwaConfig() ===========')
-dwa.dwaConfig(verbose=0, configFile="dwaConfigWC.ini")
-#dwa.dwaConfig(verbose=0, configFile="dwaConfigSingleFreq.ini")
+s = dwa.tcpOpen(verbose=False)
 
-print('\n\n======= dwaStart() ===========')
-dwa.dwaStart(verbose=1)
+# LS 24b dateCode                                                                          
+dwa.dwaRegRead(s, '00000012')
+time.sleep(sleepSec)
+# read errorBits                                                                                 
+dwa.dwaRegRead(s, '00000034')
+time.sleep(sleepSec)
+# ctrl busy                                                                                          
+dwa.dwaRegRead(s, '00000011')
+time.sleep(sleepSec)
+# constant                                                                                           
+dwa.dwaRegRead(s, '00000012')
+time.sleep(sleepSec)
+# fifoAutoDC_ff fifoAutoDC_ef                                                                        
+dwa.dwaRegRead(s, '0000001B')
+time.sleep(sleepSec)
+dwa.tcpClose(s)
+
+#print('\n\n======= dwaConfig() ===========')
+#dwa.dwaConfig(verbose=0, configFile="dwaConfigWC.ini")
+##dwa.dwaConfig(verbose=0, configFile="dwaConfigSingleFreq.ini")
+
+#print('\n\n======= dwaStart() ===========')
+#dwa.dwaStart(verbose=1)
 
 #print('\n\n======= dwaStat() ===========')
 #dwa.dwaStat(verbose=1)
