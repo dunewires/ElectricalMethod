0d3a68fe53337d5349516d6075106790d2c163f2
Nathan Felt <felt@fas.harvard.edu>
2024-05-01 00:17:08 -0400

    update gogo

--- DAQ/python/dwaGoGo.py
+++ DAQ/python/dwaGoGo.py
@@ -5,39 +5,6 @@ print('\n\n======= dwaRelayConfigTest() ===========')
 #dwa.dwaReset(verbose=1)
 sleepSec = 0.2
 s = dwa.tcpOpen(verbose=False)
-
-relayBusTopReg = ['']*2
-relayBusBotReg = ['']*2
-
-#set initial values
-relayBusTopReg[0] =  0x0
-relayBusTopReg[1] =  0xaa
-relayBusBotReg[0] =  relayBusTopReg[1] <<2
-relayBusBotReg[1] =  0x22
-print('relayBusTopReg', format(relayBusTopReg[1],'x').zfill(8),relayBusTopReg[0])
-print('relayBusTopReg', relayBusBotReg[1],relayBusBotReg[0])
-
-
-
-# LS 24b dateCode                                                                          
-dwa.dwaRegRead(s, '00000012')
-time.sleep(sleepSec)
-# read errorBits                                                                                 
-dwa.dwaRegRead(s, '00000034')
-time.sleep(sleepSec)
-
-# relayBusTop(1);
-dwa.dwaRegWrite(s, '0000002B', 'aaaaaaaa')
-time.sleep(sleepSec)
-# relayBusTop(0);
-time.sleep(sleepSec)
-# relayBusBot(1);
-dwa.dwaRegWrite(s, '00000025', '00000000')
-time.sleep(sleepSec)
-# relayBusBot(0);
-dwa.dwaRegWrite(s, '00000024', '00000000')
-time.sleep(sleepSec)
-
 # !! for now all wires are off, should we do something here?
 # relayWireTop(3);
 dwa.dwaRegWrite(s, '00000029', '00000000')
@@ -64,12 +31,54 @@ time.sleep(sleepSec)
 dwa.dwaRegWrite(s, '00000020', '00000000')
 time.sleep(sleepSec)
 
-# Update relays
-dwa.dwaRegWrite(s, '00000000', '00000004')
-time.sleep(sleepSec)
+relayBusTopReg = ['']*2
+relayBusBotReg = ['']*2
 
-# read errorBits                                                                                 
-dwa.dwaRegRead(s, '00000034')
+relayBusStart =  0x8000000000000000
+relayBusError =  0x4000000000000000
+for i in range(16):
+	relayBusScan =  relayBusStart
+	for j in range(16):
+		relayBus = relayBusError | relayBusScan
+		#print('test index',i,j, end=' ')
+		print('Bus bits',format(relayBus,'b').zfill(64), end=' ')
+		relayBusBotReg[1] =  (relayBus & 0xffff000000000000)>>48
+		relayBusBotReg[0] =  (relayBus & 0x0000ffff00000000)>>32
+		relayBusTopReg[1] =  (relayBus & 0x00000000ffff0000)>>16
+		relayBusTopReg[0] =  (relayBus & 0x000000000000ffff)
+		print('relayBusBotReg',format(relayBusBotReg[1],'x').zfill(8),format(relayBusBotReg[0],'x').zfill(8), end=' ')
+		print('relayBusTopReg',format(relayBusTopReg[1],'x').zfill(8),format(relayBusTopReg[0],'x').zfill(8))
+
+		# read errorBits                                                                                 
+		print (dwa.dwaRegRead(s, '00000034'), end=' ')
+		time.sleep(sleepSec)
+
+		# relayBusTop(1);
+		dwa.dwaRegWrite(s, '0000002B', format(relayBusTopReg[1],'x').zfill(8))
+		time.sleep(sleepSec)
+		# relayBusTop(0);
+		dwa.dwaRegWrite(s, '0000002A', format(relayBusTopReg[0],'x').zfill(8))
+		time.sleep(sleepSec)
+		# relayBusBot(1);
+		dwa.dwaRegWrite(s, '00000025', format(relayBusBotReg[1],'x').zfill(8))
+		time.sleep(sleepSec)
+		# relayBusBot(0);
+		dwa.dwaRegWrite(s, '00000024', format(relayBusBotReg[0],'x').zfill(8))
+		time.sleep(sleepSec)
+		# Update relays
+		dwa.dwaRegWrite(s, '00000000', '00000004')
+		time.sleep(sleepSec)
+
+		# read errorBits                                                                                 
+		print (dwa.dwaRegRead(s, '00000034'), end=' ')
+		time.sleep(sleepSec)
+
+
+		relayBusScan = relayBusScan>>2
+	relayBusError = relayBusError>>2
+
+# LS 24b dateCode                                                                          
+dwa.dwaRegRead(s, '00000012')
 time.sleep(sleepSec)
 
 dwa.tcpClose(s)
